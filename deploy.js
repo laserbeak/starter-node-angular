1344360535947da2af63e9ceefadd9d9e9a7dd43
Ubuntu <ubuntu@ip-172-31-27-16.ec2.internal>
2019-09-03 22:19:40 +0000

    aaaand again

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '172-31-27-16',
+      host: '172.31.27.16',
       username: 'ubuntu',
       privateKey: 'fp-key.pem'
     })
